39c1396058de49a3834cea9d680d44f010314540
krishna sai <krishsai0303@gmail.com>
2025-04-27 18:40:33 +0530

    please work

--- server/db/client.ts
+++ server/db/client.ts
@@ -1,7 +1,15 @@
-// client.ts
 import { createClient } from "@libsql/client";
 
+const url = Deno.env.get("TURSO_DB_URL");
+const authToken = Deno.env.get("TURSO_DB_TOKEN");
+
+if (!url || !authToken) {
+  throw new Error(
+    "Missing TURSO_DB_URL or TURSO_DB_TOKEN environment variables."
+  );
+}
+
 export const db = createClient({
-  url: Deno.env.get("TURSO_DB_URL"),
-  authToken: Deno.env.get("TURSO_DB_TOKEN"),
+  url,
+  authToken,
 });
