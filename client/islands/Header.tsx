805bb496ae5d13e51e70c7484168fef51653a09e
krishna sai <krishsai0303@gmail.com>
2025-04-27 14:16:34 +0530

    added feedback button and made it responsive and added a modal for feedback

--- client/islands/Header.tsx
+++ client/islands/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
     <header class="relative bg-gray-800 text-white p-4 flex justify-between items-center">
-      <div class="font-sans font-semibold">Zapreach</div>
+      <div class="flex items-center">
+        <img src="/icon3.png" alt="app logo" class="w-10 h-10 mr-1" />
+        <span class="font-sans font-semibold ml-1 text-3xl">Zapreach</span>
+      </div>
       <nav>{/* Your nav links */}</nav>
 
       {/* Mobile button: absolute in header, visible only on small screens */}
