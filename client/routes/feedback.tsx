1f7dbc8fd723503cfe572928504227d1787016d2
krishna sai <krishsai0303@gmail.com>
2025-04-27 20:20:47 +0530

    removed client side code

--- client/routes/feedback.tsx
+++
@@ -1,9 +0,0 @@
-import FeedbackForm from "../islands/FeebackForm.tsx";
-
-export default function FeedbackPage() {
- return (
-  <div class="min-h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950">
-    <FeedbackForm />
-  </div>
-  ); 
-}
